fa8b86373df214970bf33ecea53f0986863e0761
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-06-23 12:44:54 +0900

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
+<<<<<<< HEAD
+<<<<<<< Updated upstream
   
+=======
+  validates_presence_of(:title)
+>>>>>>> Stashed changes
+=======
+  validates :title, presence: true
+>>>>>>> 29b43790f1780a926854be91cabf0941eb1b3331
 end
